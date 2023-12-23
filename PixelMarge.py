c7b0bb669e0400ac09025d9fb0aee08ad664bf75
DrbA124 <dombowns@gmail.com>
2023-12-23 19:23:46 +0000

    Added BuildingOrderBot!

--- PixelMarge.py
+++ PixelMarge.py
@@ -13,11 +13,14 @@ from HelpfulAIs.BuildingBot import BuildingBot
 class PixelMargeBot(TrainingBot, BuildingOrderBot, BuildingBot):
     async def on_step(self, iteration):
         if iteration == 0:
+            self.IsFirstStep = True
             await self.chat_send("GG EZ")
+        else: self.IsFirstStep = False
         
         #await self.ControlWorkers()
         await self.TimeIt(self.distribute_workers, "distribute_workers")
-        
+        await self.TimeIt(self.TrainWorkers, "TrainWorkers")
+        await self.TimeIt(self.ExecuteBuildOrder, "ExecuteBuildOrder")
             
         self.GetDebugInfo(True, Iteration = iteration)
         
