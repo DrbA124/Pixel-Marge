27ec5253bc8c4f9027559cefc1c6a794c3a05a2c
PJneMk184 <154686005+PJneMk184@users.noreply.github.com>
2023-12-23 15:28:03 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import sc2
+import random
+from sc2.data import Race, Difficulty
+from sc2.main import run_game
+from sc2.player import Bot, Human, Computer
+from sc2 import maps
+from import
+bot = Bot(Race.Zerg, ZergBot())
+bot2 = Bot(Race.Terran, MMTM())
+human = Human(Race.Zerg, fullscreen=True)
+ai = Computer(Race.Zerg, Difficulty.VeryHard)
+
+def main():
+    mapit = [
+        "AcropolisLE",
+    ]
+    map_name = random.choice(mapit)
+
+    run_game(
+        maps.get(map_name),
+        [bot, ai],
+        realtime=False
+    )
+
+
+if __name__ == '__main__':
+    main()
