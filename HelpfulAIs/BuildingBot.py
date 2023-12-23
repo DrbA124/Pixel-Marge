6a3fa84b4e483f26750a4ee724398d2c5c94ec5c
PJneMk184 <norriner@gmail.com>
2023-12-23 19:55:05 +0000

    BuildingBot updates

--- HelpfulAIs/BuildingBot.py
+++ HelpfulAIs/BuildingBot.py
@@ -4,9 +4,26 @@ from sc2.data import Difficulty, Race
 from sc2.ids.unit_typeid import UnitTypeId
 from sc2.ids.ability_id import AbilityId
 from sc2.unit import Unit
+from sc2.position import Point2
 
 from HelpfulAIs.GeneralUtilsAI import GeneralUtilsAI
 
 #Make Pylons
 class BuildingBot(GeneralUtilsAI):
-    pass
+    def FindPylonLocations(self, Base: Unit | Point2, Distance: int = 6):
+        if self.Bases:
+            for Base in self.Bases:
+                PatchesCentre = self.GetCentreOfBasePatches(Base)
+                if PatchesCentre:
+                    return self.AwayFrom(Base, PatchesCentre, Distance)
+
+    def BuildFirstPylon(self):
+        if (
+            self.supply_workers == 15 and
+            self.can_afford(UnitTypeId.PYLON) and
+            self.structures(UnitTypeId.PYLON).amount == 0
+        ):
+            await self.build(UnitTypeId.PYLON, location)
+        elif (
+
+        ):
