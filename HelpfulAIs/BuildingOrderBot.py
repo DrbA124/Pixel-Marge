99d7e509630d82a5c9e9a39ab47fa3035f920022
DrbA124 <107809517+DrbA124@users.noreply.github.com>
2023-12-23 18:23:52 +0000

    AddedBuildng bot

---
+++ HelpfulAIs/BuildingOrderBot.py
@@ -0,0 +1,25 @@
+from sc2 import maps
+from sc2.bot_ai import BotAI
+from sc2.data import Difficulty, Race
+from sc2.ids.unit_typeid import UnitTypeId
+from sc2.ids.ability_id import AbilityId
+from sc2.unit import Unit
+
+from HelpfulAIs.ProtossUtilsAI import ProtossUtilsAI
+
+class BuildingOrderBot(ProtossUtilsAI):
+    def SelectBuildOrder(self):
+        BuildOrderOptions = [
+            """
+            13	  0:18	  Pylon	  
+            15	  0:37	  Gateway	  
+            16	  0:48	  Assimilator	  
+            19	  1:22	  Nexus	  
+            21	  1:37	  Cybernetics Core	  
+            21	  1:45	  Assimilator	  
+            22	  1:51	  Pylon	  
+            23	  2:15	  Adept, Warp Gate	  Scout with this
+            26	  2:30	  Pylon	  
+            27	  2:37	  Stargate
+        """]
+        print(BuildOrderOptions[0])
